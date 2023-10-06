3f23141bfc180e234e19cb17825d0025ca24bb6e
babs3 <barbaraema3@gmail.com>
2023-10-06 17:52:19 +0100

    Shared Queue (DEALER and ROUTER sockets) donee

---
+++ SharedQueuepattern/Broker.c
@@ -0,0 +1,54 @@
+//  Simple request-reply broker
+// The broker acts as an intermediary that forwards messages between the client and the worker.
+
+#include "zhelpers.h" 
+
+int main (void) 
+{
+    //  Prepare our context and sockets
+    void *context = zmq_ctx_new ();
+    void *frontend = zmq_socket (context, ZMQ_ROUTER);
+    void *backend  = zmq_socket (context, ZMQ_DEALER);
+    zmq_bind (frontend, "tcp://*:5559"); // Binds a ROUTER socket to tcp://*:5559 (client connection)
+    zmq_bind (backend,  "tcp://*:5560"); // Binds a DEALER socket to tcp://*:5560 (worker connection)
+
+    //  Initialize poll set
+    zmq_pollitem_t items [] = {
+        { frontend, 0, ZMQ_POLLIN, 0 },
+        { backend,  0, ZMQ_POLLIN, 0 }
+    };
+    //  Switch messages between sockets
+    while (1) {
+        zmq_msg_t message;
+        zmq_poll (items, 2, -1);
+        if (items [0].revents & ZMQ_POLLIN) {
+            while (1) {
+                //  Process all parts of the message
+                zmq_msg_init (&message);
+                zmq_msg_recv (&message, frontend, 0);
+                int more = zmq_msg_more (&message);
+                zmq_msg_send (&message, backend, more? ZMQ_SNDMORE: 0);
+                zmq_msg_close (&message);
+                if (!more)
+                    break;      //  Last message part
+            }
+        }
+        if (items [1].revents & ZMQ_POLLIN) {
+            while (1) {
+                //  Process all parts of the message
+                zmq_msg_init (&message);
+                zmq_msg_recv (&message, backend, 0);
+                int more = zmq_msg_more (&message);
+                zmq_msg_send (&message, frontend, more? ZMQ_SNDMORE: 0);
+                zmq_msg_close (&message);
+                if (!more)
+                    break;      //  Last message part
+            }
+        }
+    }
+    //  We never get here, but clean up anyhow
+    zmq_close (frontend);
+    zmq_close (backend);
+    zmq_ctx_destroy (context);
+    return 0;
+}
